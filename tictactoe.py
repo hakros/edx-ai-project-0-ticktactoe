65292cb8476cbbad087b2a39fcdd58bae88e1b39
Hakeem <hakeemrosario@gmail.com>
2024-06-13 14:43:41 +0000

    Implementation of minimax algo Added recurseMin function Added recurseMax function Added implementation of minimax function Modified actions to use set instead of list

--- tictactoe.py
+++ tictactoe.py
@@ -3,6 +3,7 @@ Tic Tac Toe Player
 """
 
 import math
+import copy
 
 X = "X"
 O = "O"
@@ -25,13 +26,22 @@ def player(board):
     player1_moves = 0
     player2_moves = 0
 
+    # Calculate the number of squares on the xAxis and yAxis of the board
     xLen = len(board[0])
     yLen = len(board)
 
+    # Calcualte how many squares in total the board has
     boardArea = xLen * yLen
 
+    # Loop through boardArea
+    # For every X that we find, we add that to the number of moves that player1 has made
+    # For every O that we find, we add that to the number of moves that player 2 has made
     for i in range(boardArea):
+        # Division without decimal to find where we are i the y-axis
         yPos = i // yLen
+
+        # Use modulo operation to find the remainder after division
+        # This is our position in the x axis
         xPos = i % xLen
 
         if board[yPos][xPos] == X:
@@ -39,6 +49,8 @@ def player(board):
         elif board[yPos][xPos] == O:
             player2_moves += 1
 
+    # If player 1 has more moves than player 2, it is player 1s turn
+    # Else it is player 2s turn
     return O if player1_moves > player2_moves else X
 
 
@@ -46,23 +58,32 @@ def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    possibleActions = []
+    possibleActions = set()
 
+    # Calculate the number of squares on the xAxis and yAxis of the board
     xLen = len(board[0])
     yLen = len(board)
 
+    # Calcualte how many squares in total the board has
     boardArea = xLen * yLen
 
+    # Loop through boardArea
     for i in range(boardArea):
+        # Division without decimal to find where we are i the y-axis
         yPos = i // yLen
+
+        # Use modulo operation to find the remainder after division
+        # This is our position in the x axis
         xPos = i % xLen
 
+        # If the square is not empty, this is not a possible move
         if board[yPos][xPos] != EMPTY:
             continue
 
-        possibleActions.append((yPos, xPos))
+        possibleActions.add((yPos, xPos))
 
     return possibleActions
+
 
 def result(board, action):
     """
@@ -70,10 +91,18 @@ def result(board, action):
     """
     # The player that made the move
     playerToMove = player(board)
+    possibleMoves = actions(board)
 
-    board[action[0]][action[1]] = playerToMove
+    # Create a deep copy of the board to avoid modifying the original board
+    newBoard = copy.deepcopy(board)
 
-    return board
+    # Check if the action or move is a valid move
+    if action not in possibleMoves or newBoard[action[0]][action[1]] != EMPTY:
+        raise Exception
+
+    newBoard[action[0]][action[1]] = playerToMove
+
+    return newBoard
 
 
 def winner(board):
@@ -82,16 +111,16 @@ def winner(board):
     """
     # A list of winning moves
     winningMoves = [
-        [(0,0),(0,1),(0,2)],
-        [(1,0),(1,1),(1,2)],
-        [(2,0),(2,1),(2,2)],
+        [(0, 0), (0, 1), (0, 2)],
+        [(1, 0), (1, 1), (1, 2)],
+        [(2, 0), (2, 1), (2, 2)],
 
-        [(0,0),(1,0),(2,0)],
-        [(0,1),(1,1),(2,1)],
-        [(0,2),(1,2),(2,2)],
+        [(0, 0), (1, 0), (2, 0)],
+        [(0, 1), (1, 1), (2, 1)],
+        [(0, 2), (1, 2), (2, 2)],
 
-        [(0,0),(1,1),(2,2)],
-        [(0,2),(1,1),(2,0)],
+        [(0, 0), (1, 1), (2, 2)],
+        [(0, 2), (1, 1), (2, 0)],
     ]
 
     # Our winning conditions
@@ -159,8 +188,107 @@ def utility(board):
     return -1 if newWinner == O else 1
 
 
+def recurseMin(state, depth):
+    """A recursive function that finds the lowest number of move before a win"""
+    # Stop recursion once the game is over
+    if terminal(state) is True:
+        return depth
+
+    possibleActions = actions(state)
+
+    # Since we are finding the min value, we set the initial value as high as possible
+    value = math.inf
+
+    for possibleAction in possibleActions:
+        newBoard = result(
+            state,
+            possibleAction
+        )
+
+        # Find the maximum number of moves before a win if this move is used
+        # Every recursion, we add 1 to the depth or number of moves made
+        newValue = recurseMax(newBoard, depth+1)
+
+        # Use the lowest number of move between value and newValue
+        value = min(value, newValue)
+
+    # Return the lowest number of move until win
+    return value
+
+
+def recurseMax(state, depth):
+    """A recursive function that finds the highest number of move before a win"""
+    # Stop recursion once the game is over
+    if terminal(state) is True:
+        return depth
+
+    possibleActions = actions(state)
+
+    # Since we are finding the min value, we set the initial value as low as possible
+    value = -(math.inf)
+
+    for possibleAction in possibleActions:
+        newBoard = result(
+            state,
+            possibleAction
+        )
+
+        # Find the minimum number of moves before a win if this move is used
+        # Every recursion, we add 1 to the depth or number of moves made
+        newValue = recurseMin(newBoard, depth+1)
+
+        # Use the highest number of move between value and newValue
+        value = max(value, newValue)
+
+    # Return the highest number of move until win
+    return value
+
+
 def minimax(board):
     """
     Returns the optimal action for the current player on the board.
     """
-    raise NotImplementedError
+    # No more possible moves to make
+    if terminal(board) is True:
+        return None
+
+    playerToMove = player(board)
+    possibleActions = actions(board)
+
+    # Stores the best move to use
+    moveToUse = ()
+
+    # Stores the number of moves until win
+    # O is the min player so we set initial value as low as possible for him
+    # X is the max player so we set initial value as high as possible for him
+    # max player is always the first to move
+    value = -(math.inf) if playerToMove == O else math.inf
+
+    for possibleAction in possibleActions:
+        newBoard = result(
+            board,
+            possibleAction
+        )
+
+        if playerToMove == O:
+            # Find the lowest number of move before we lose, if we use this move
+            newValue = recurseMin(newBoard, 0)
+
+            # If this move gives us the lower chance to lose compared to the move stored in moveToUse,
+            # This is the new best move
+            # Example: 5 > 4 moves. 5 moves before lose is better than 4 moves before lose
+            if (newValue > value):
+                value = newValue
+                moveToUse = possibleAction
+        elif playerToMove == X:
+            # Find the highest number of moves before we win, if we use this move
+            newValue = recurseMax(newBoard, 0)
+
+            # If this move gives us the higher chance to win compared to the move stored in moveToUse,
+            # This is the new best move
+            # Example 4 < 5 moves. 4 moves before win is better than 5 moves before win
+            if (newValue < value):
+                value = newValue
+                moveToUse = possibleAction
+
+    return moveToUse
